0a549936ade362b772500a9c8626fe3d71fe8d21
DylanMoon <43023068+DylanMoon@users.noreply.github.com>
2020-04-03 21:03:24 -0700

    Add files via upload

--- BasicDictionary.java
+++ BasicDictionary.java
@@ -3,7 +3,6 @@ package spellchecker;
 import static sbcc.Core.*;
 import static java.lang.Math.*;
 import static java.lang.System.*;
-
 import java.io.*;
 import java.util.*;
 
@@ -24,10 +23,35 @@ public class BasicDictionary implements Dictionary {
 		if (start > end) {
 			return;
 		}
-		int middle = (int) round((start + end) / 2.0);
+		int middle = (int) ceil((start + end) / 2.0);
 		add(list.get(middle));
-		recursiveImportHelper(list, start, middle - 1);
 		recursiveImportHelper(list, middle + 1, end);
+		recursiveImportHelper(list, start, middle - 1);
+
+	}
+
+
+	// @Override
+	public void importFile2(String filename) throws Exception {
+		if (tree.getRoot() != null) {
+			tree.clear();
+		}
+		List<String> words = readFileAsLines(filename);
+		recursiveImportHelper2(words, 0, words.size() - 1);
+	}
+
+
+	private void recursiveImportHelper2(List<String> list, int start, int end) {
+		if (start > end) {
+			return;
+		}
+
+		int root = start + getRoot(end - start + 1);
+		// add(list.get(root));
+		println(list.get(root));
+		recursiveImportHelper2(list, start, root - 1);
+		recursiveImportHelper2(list, root + 1, end);
+
 	}
 
 
@@ -128,6 +152,50 @@ public class BasicDictionary implements Dictionary {
 
 	public void clear() {
 		tree.clear();
+	}
+
+
+	private int getRoot(int nodes) {
+		var index = 0;
+		var completeRows = nCompleteRows(nodes);
+		var orphans = leftOverNodes(nodes);
+		for (int i = 0; i < completeRows - 1; i++) {// runs n * 2 + 1 a complete number of rows -1 times
+			index = (index * 2) + 1;
+		}
+		if (orphans == 0) {// if building a full tree return the root node
+			return index;
+		}
+		// if the number of orphan nodes is greater or equal to the half of the final
+		// row complete one more cycle of n * 2 + 1
+		// if the number of orphan nodes is less than half of the final row offset the
+		// root index by the number of orphans
+		return ((((int) pow(2, completeRows)) / 2) <= orphans) ? (index * 2) + 1 : index + orphans;
+	}
+
+
+	private int nCompleteRows(int nodes) {
+		var rows = 0;
+		while (nodes >= pow(2, rows) - 1) {
+			rows++;
+		}
+		return rows - 1;
+	}
+
+
+	private int leftOverNodes(int nodes) {
+		if (nodes == 0) {
+			return 0;
+		}
+		var level = levels(nodes);
+		return (((int) pow(2, level) - 1) == nodes) ? 0 : (nodes - ((int) pow(2, level - 1) - 1));
+	}
+
+
+	private int levels(int x) {
+		if (x == 0) {
+			return 0;
+		}
+		return (int) (floor(log10(x) / log10(2))) + 1;
 	}
 
 }
--- Main.java
+++ Main.java
@@ -17,77 +17,11 @@ import static org.apache.commons.lang3.StringUtils.*;
  *
  */
 public class Main {
-	static final int NUM_NODES = 175169;
 
 	public static void main(String[] args) throws Exception {
-		var lastL = 0;
-		printf("%10s%15s\n", "Nodes", "Levels");
-		for (int i = 1; i <= NUM_NODES; i = i * 2) {
-			var level = levels(i);
-			if (level > lastL) {
-				lastL = level;
-				printf("%10s%14s\n", i - 1, level - 1);
-			}
-		}
-		var lastRoot = 0;
-		for (int i = 0; i <= NUM_NODES; i++) {
-			var root = getRoot(i);
-			if (root > lastRoot) {
-				lastRoot = root;
-				println("Root index for a tree with " + i + " nodes is: " + root);
-			}
-		}
-		println("Wrong root for a tree with " + NUM_NODES + " nodes is: " + ((int) ceil(NUM_NODES / 2)));
 
-	}
+		BasicDictionary bd = new BasicDictionary();
+		bd.importFile2("my_test_alpha.txt");
 
-
-	public static int getRoot(int nodes) {
-		var index = 0;
-		var completeRows = nCompleteRows(nodes);
-		var level = levels(nodes);
-		var orphans = leftOverNodes(nodes);
-		for (int i = 0; i < completeRows - 1; i++) {// need to loop complete rows -1
-			index = (index * 2) + 1;
-		}
-		if (orphans == 0) {
-			return index;
-		}
-		return ((((int) pow(2, completeRows)) / 2) <= orphans) ? (index * 2) + 1 : index + orphans;
-	}
-
-
-	public static int nCompleteRows(int nodes) {
-		var rows = 0;
-		while (nodes >= pow(2, rows) - 1) {
-			rows++;
-		}
-		return rows - 1;
-	}
-
-
-	public static int getTreeDepth(BinaryTreeNode node) {
-		if (node == null)
-			return 0;
-		else
-			return 1 + max(getTreeDepth(node.left), getTreeDepth(node.right));
-	}
-
-
-	public static int leftOverNodes(int nodes) {
-		if (nodes == 0) {
-			return 0;
-		} else {
-			var level = levels(nodes);
-			return (((int) pow(2, level) - 1) == nodes) ? 0 : (nodes - ((int) pow(2, level - 1) - 1));
-		}
-	}
-
-
-	public static int levels(int x) {
-		if (x == 0) {
-			return 0;
-		}
-		return (int) (floor(log10(x) / log10(2))) + 1;
 	}
 }
